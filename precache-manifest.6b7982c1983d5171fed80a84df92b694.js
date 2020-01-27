3c3716f7475ac7d61de51e3c7c798a23275c6878
Aman <aman@ratiocinativesolutions.com>
2020-01-27 21:05:07 +0530

    Updates

---
+++ precache-manifest.6b7982c1983d5171fed80a84df92b694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21a0a05d5bab491e99eea64c3f4214a",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "3b7704cf9285f48e37d1",
+    "url": "/burger/static/css/main.ff459380.chunk.css"
+  },
+  {
+    "revision": "5e9775cdcddab179ee02",
+    "url": "/burger/static/js/2.997a54fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger/static/js/2.997a54fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b7704cf9285f48e37d1",
+    "url": "/burger/static/js/main.f6b59bc3.chunk.js"
+  },
+  {
+    "revision": "7c8600a64a0f3965c10e",
+    "url": "/burger/static/js/runtime-main.2571d9b3.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
